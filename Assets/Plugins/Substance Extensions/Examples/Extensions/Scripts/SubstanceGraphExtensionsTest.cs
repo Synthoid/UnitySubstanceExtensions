0d82b42064a26774efe5f553117c0d7b97bad089
Christopher Ingerson <chrisdingerson@gmail.com>
2018-06-29 18:03:07 -0400

    Added SubstanceGraphExtensions class to contain extension methods for SubstanceGraphs.

---
+++ Assets/Plugins/Substance Extensions/Examples/Extensions/Scripts/SubstanceGraphExtensionsTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Substance.Game;
+
+namespace Substance.Examples
+{
+	public class SubstanceGraphExtensionsTest : MonoBehaviour
+	{
+        [SerializeField]
+        [Tooltip("The SubstanceGraph to randomize the random seed value for.")]
+        private SubstanceGraph graph;
+        [SerializeField]
+        [Tooltip("Key to press to randomize the random seed value of a graph and rebuild it.")]
+        private KeyCode randomizeKey = KeyCode.Space;
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(randomizeKey))
+            {
+                graph.RandomizeSeed();
+                graph.QueueForRender();
+            }
+        }
+    }
+}
---
+++ Assets/Plugins/Substance Extensions/Extensions/SubstanceGraphExtensions.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Substance.Game
+{
+    /// <summary>
+    /// Class containing extension methods for the <see cref="SubstanceGraph"/> class.
+    /// </summary>
+	public static class SubstanceGraphExtensions
+	{
+        /// <summary>
+        /// Name for the random seed parameter on <see cref="SubstanceGraph"/> assets.
+        /// </summary>
+        public const string RandomSeedParameter = "$randomseed";
+
+        /// <summary>
+        /// Randomizes and returns the random seed value for a <see cref="SubstanceGraph"/> asset.
+        /// </summary>
+        /// <param name="graph">The graph to randomize the seed value for.</param>
+		public static int RandomizeSeed(this SubstanceGraph graph)
+        {
+            int newSeed = Random.Range(int.MinValue, int.MaxValue);
+
+            graph.SetInputInt(RandomSeedParameter, newSeed);
+
+            return newSeed;
+        }
+
+        /// <summary>
+        /// Set the random seed value for a <see cref="SubstanceGraph"/> asset. Returns the new seed value.
+        /// </summary>
+        /// <param name="graph">The <see cref="SubstanceGraph"/> to set the random seed value for.</param>
+        /// <param name="seed">Value to set the random seed to.</param>
+        public static int SetRandomSeed(this SubstanceGraph graph, int seed)
+        {
+            graph.SetInputInt(RandomSeedParameter, seed);
+
+            return seed;
+        }
+	}
+}
